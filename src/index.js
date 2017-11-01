b4823b23aae11285fd03167f8c6c8888730b6533
Koichi Miyamoto <m5813k@gmail.com>
2017-11-02 03:16:53 +0900

    変数名をそれっぽいものに変更

--- src/index.js
+++ src/index.js
@@ -6,49 +6,49 @@ window.onload = function(){
   window.addEventListener('keydown', k, true);
   function k(h){e = (h.keyCode !== 27);}
 
-  const ccc = document.querySelector('#canvas');
-  const ctx = ccc.getContext('webgl');
-  const ppp = ctx.createProgram();
+  const cvs = document.querySelector('#canvas');
+  const ctx = cvs.getContext('webgl');
+  const pgm = ctx.createProgram();
 
   // シェーダ生成関数
   const hhh = function(i, j){
     k = ctx.createShader(ctx.VERTEX_SHADER - i);
     ctx.shaderSource(k, j);
     ctx.compileShader(k);
-    ctx.attachShader(ppp, k);
+    ctx.attachShader(pgm, k);
     return ctx.getShaderInfoLog(k);
   }
 
-  if(!hhh(0, vert) && !hhh(1, frag)){ctx.linkProgram(ppp);}
+  if(!hhh(0, vert) && !hhh(1, frag)){ctx.linkProgram(pgm);}
 
-  const eee = ctx.getProgramParameter(ppp, ctx.LINK_STATUS);
-  ctx.useProgram(ppp);
+  const isLinked = ctx.getProgramParameter(pgm, ctx.LINK_STATUS);
+  ctx.useProgram(pgm);
 
   const uuu = {};
-  uuu.time = ctx.getUniformLocation(ppp, 'time');
-  uuu.resolution = ctx.getUniformLocation(ppp, 'resolution');
+  uuu.time = ctx.getUniformLocation(pgm, 'time');
+  uuu.resolution = ctx.getUniformLocation(pgm, 'resolution');
   ctx.bindBuffer(ctx.ARRAY_BUFFER, ctx.createBuffer());
   ctx.bufferData(ctx.ARRAY_BUFFER, new Float32Array([-1,1,0,-1,-1,0,1,1,0,1,-1,0]), ctx.STATIC_DRAW);
-  const aaa = ctx.getAttribLocation(ppp, 'position');
-  ctx.enableVertexAttribArray(aaa);
-  ctx.vertexAttribPointer(aaa, 3, ctx.FLOAT, false, 0, 0);
+  const atr = ctx.getAttribLocation(pgm, 'position');
+  ctx.enableVertexAttribArray(atr);
+  ctx.vertexAttribPointer(atr, 3, ctx.FLOAT, false, 0, 0);
   ctx.clearColor(0, 0, 0, 1);
 
   // 無名関数でメインルーチンを実行
   function update(milli) {
-    if(!eee){return;} // シェーダのリンクに失敗していたら実行しない
+    if(!isLinked){return;} // シェーダのリンクに失敗していたら実行しない
 
     // ビューポートを動的に指定する
-    ccc.width  = window.innerWidth;
-    ccc.height = window.innerHeight;
-    ctx.viewport(0, 0, ccc.width, ccc.height);
+    cvs.width  = window.innerWidth;
+    cvs.height = window.innerHeight;
+    ctx.viewport(0, 0, cvs.width, cvs.height);
 
     // フレームバッファをクリア
     ctx.clear(ctx.COLOR_BUFFER_BIT);
 
     // uniform変数をプッシュ
     ctx.uniform1f(uuu.time, milli * 0.001);
-    ctx.uniform2fv(uuu.resolution, [ccc.width, ccc.height]);
+    ctx.uniform2fv(uuu.resolution, [cvs.width, cvs.height]);
 
     // プリミティブのレンダリング
     ctx.drawArrays(ctx.TRIANGLE_STRIP, 0, 4);
